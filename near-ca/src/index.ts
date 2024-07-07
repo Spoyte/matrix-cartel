5855f2053cf4c6e481febce50bbc98359b760f8a
spoyte <noe.de.91@gmail.com>
2024-07-07 13:27:25 +0200

    cleanup

--- near-ca/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./chains/ethereum";
-export * from "./chains/near";
-export * from "./mpcContract";
-export * from "./types/types";
-export * from "./utils/signature";
-export * from "./network";
